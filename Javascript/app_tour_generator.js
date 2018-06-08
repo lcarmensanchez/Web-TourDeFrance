a5b4341e790703b27ac000d94ab2839eb3c45c01
lesanche <lesanche@student.42.fr>
2018-06-08 22:36:59 +0200

    test

--- Javascript/app_tour_generator.js
+++ Javascript/app_tour_generator.js
@@ -1,21 +1,27 @@
 function searchInJson(year){
-  		var data = getJSON()
+      var data = getJSON()
+      // d3.json("/Resources/results2.json", function(data) {
+      //   console.log(data[0]);
+      // });
+      
         $('#winner').html("");
         $('#climber').html("");
         $('#sprinter').html("");
         $('#other-result').html("");
 
         if ((year >= 1903 && year < 1915) || (year > 1918 && year < 1940) || (year > 1946 && year <= 2017))  {
-            if (data[year].Winner.name == 'd\u00e9sattribu\u00e9')
+            if (data[year].Winner.name == 'd\u00e9sattribu\u00e9'){
                 $('#winner').html('Pas de vainqueur : l\'américain Lance Armstrong a remport\u00e9 sept Tours entre 1999 et 2005. Mais ils lui ont été retirés en 2012 par l\'Union cycliste internationale. Cette année-là, l\'Agence américaine antidopage (USADA) conclut en effet que le coureur a utilisé des substances dopantes pour améliorer ses performances sportives.');
+            }
             else
-                $('#winner').html('Gagnant du Tour de France : <b>' + data[year].Winner.name + '</b> (' + data[year].Winner.nationality + ')');
+                $('#winner').html('<a class="img col-xs-12"> Gagnant du Tour de France : <b>' + data[year].Winner.name + '</b> (' + data[year].Winner.nationality + ')</a>');
             if (data[year].Winner.name != 'none' && data[year].Climber.nationality != "none")
                 $('#climber').html('Meilleur grimpeur : <b>' + data[year].Climber.name + '</b> (' + data[year].Climber.nationality + ')');
             if (year < 1953)
                 $('#sprinter').html("Pas de meilleur sprinter : le maillot vert n'a été créé qu\'en 1953, cinquante ans après le premier Tour de France.");
             else
                 $('#sprinter').html('Meilleur sprinteur : <b>' + data[year].Sprinter.name + '</b> (' + data[year].Sprinter.nationality + ')');
+            $('#winner').prepend('<img class="img col-xs-1" src="./Resources/img/MAILLOTJAUNE.png" />')
         }
         else if (year >= 1915 && year <= 1918) 
             $('#other-result').html("De 1915 à 1918, il n'y a pas eu de Tour de France à cause de la Première Guerre mondiale.");
