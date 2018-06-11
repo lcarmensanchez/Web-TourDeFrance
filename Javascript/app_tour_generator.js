54f9b67d3b2d3b963e0017180d0fc18667ce8315
lcarmensanchez <lea.sanchez@sciencespo.fr>
2018-06-11 22:19:25 +0200

    explanations

--- Javascript/app_tour_generator.js
+++ Javascript/app_tour_generator.js
@@ -12,22 +12,35 @@ function searchInJson(year){
             }
             else {
 				$('#winner').html('<a> Gagnant du Tour de France : <b>' + data[year].Winner.name + '</b> (' + data[year].Winner.nationality + ')</a>');
-				$('#winner').prepend('<img class="img" src="./Resources/img/MAILLOTJAUNE.png" />')
+				$('#winner').prepend('<img class="img" id="maillot_jaune" src="./Resources/img/MAILLOTJAUNE.png" />')
 			}
 			if (data[year].Winner.name != 'none' && data[year].Climber.nationality != "none")
-                $('#climber').html('<img class="img" src="./Resources/img/MAILLOTAPOIS.png" />Meilleur grimpeur : <b>' + data[year].Climber.name + '</b> (' + data[year].Climber.nationality + ')');
+                $('#climber').html('<img class="img" src="./Resources/img/MAILLOTAPOIS.png" id="maillot_pois" />Meilleur grimpeur : <b>' + data[year].Climber.name + '</b> (' + data[year].Climber.nationality + ')');
             if (year < 1953)
                 $('#sprinter').html("Pas de meilleur sprinter : le maillot vert n'a été créé qu\'en 1953, cinquante ans après le premier Tour de France.");
             else
-                $('#sprinter').html('<img class="img" src="./Resources/img/MAILLOTVERT.png" />Meilleur sprinteur : <b>' + data[year].Sprinter.name + '</b> (' + data[year].Sprinter.nationality + ')');
+                $('#sprinter').html('<img class="img" id="maillot_vert" src="./Resources/img/MAILLOTVERT.png" />Meilleur sprinteur : <b>' + data[year].Sprinter.name + '</b> (' + data[year].Sprinter.nationality + ')');
         }
         else if (year >= 1915 && year <= 1918) 
             $('#other-result').html("De 1915 à 1918, il n'y a pas eu de Tour de France à cause de la Première Guerre mondiale.");
         else if (year >= 1940 && year <= 1946) 
             $('#other-result').html("De 1940 à 1946, il n'y a pas eu de Tour de France à cause de la Seconde Guerre mondiale.");
-        else if (year >= 2017 || year < 1903) 
-            $('#other-result').html("H\u00e9las, le Tour de France n'a eu lieu qu'entre 1903 et 2017.");
+        $(document).ready(function(){
+            $("img").hover(function(){
+                if (this.id == "maillot_jaune")
+                    $('#explanations').html("<p class='explanations'>Depuis 1919, le coureur en tête du classement général du Tour porte le maillot jaune. Le classement est obtenu en cumulant les temps réalisés sur chaque étape par chacun des participants.</p>");
+                else if (this.id == "maillot_pois")
+                    $('#explanations').html("<p class='explanations'>Lorsqu'ils franchissent des passages escarpés comme des cols, les coureurs gagnent des points. Le meilleur grimpeur remporte le Grand Prix de la montagne et arbore un maillot blanc à pois rouges.</p>"); 
+                else if (this.id == "maillot_vert")
+                     $('#explanations').html("<p class='explanations'>Le maillot vert a été créé en 1953. Lors du sprint final de chaque étape, les coureurs remportent des points variant selon leur classement. Celui qui atteint la première place revêt le maillot vert.</p>");
+            });
+            $("img").mouseleave(function(){
+                $('#explanations').html("");
+            });
+    });
     }
+
     $( "#generator_button" ).click(function() {
         searchInJson($( "#year" ).val());
     });
+
