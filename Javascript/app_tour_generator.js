2a1ffa9db7dd0c0983511ad435762862235b0009
Léa Sanchez <lsanchez@SGRA11M.local>
2018-06-11 18:24:47 +0200

    minor changes in CSS - padding result

--- Javascript/app_tour_generator.js
+++ Javascript/app_tour_generator.js
@@ -11,7 +11,7 @@ function searchInJson(year){
                 $('#winner').html('Pas de vainqueur : l\'américain Lance Armstrong a remport\u00e9 sept Tours entre 1999 et 2005. Mais ils lui ont été retirés en 2012 par l\'Union cycliste internationale. Cette année-là, l\'Agence américaine antidopage (USADA) conclut en effet que le coureur a utilisé des substances dopantes pour améliorer ses performances sportives.');
             }
             else {
-				$('#winner').html('<a class="img"> Gagnant du Tour de France : <b>' + data[year].Winner.name + '</b> (' + data[year].Winner.nationality + ')</a>');
+				$('#winner').html('<a> Gagnant du Tour de France : <b>' + data[year].Winner.name + '</b> (' + data[year].Winner.nationality + ')</a>');
 				$('#winner').prepend('<img class="img" src="./Resources/img/MAILLOTJAUNE.png" />')
 			}
 			if (data[year].Winner.name != 'none' && data[year].Climber.nationality != "none")
@@ -19,7 +19,7 @@ function searchInJson(year){
             if (year < 1953)
                 $('#sprinter').html("Pas de meilleur sprinter : le maillot vert n'a été créé qu\'en 1953, cinquante ans après le premier Tour de France.");
             else
-                $('#sprinter').html('<img class="img" src="./Resources/img/MAILLOTVERT.png" /> Meilleur sprinteur : <b>' + data[year].Sprinter.name + '</b> (' + data[year].Sprinter.nationality + ')');
+                $('#sprinter').html('<img class="img" src="./Resources/img/MAILLOTVERT.png" />Meilleur sprinteur : <b>' + data[year].Sprinter.name + '</b> (' + data[year].Sprinter.nationality + ')');
         }
         else if (year >= 1915 && year <= 1918) 
             $('#other-result').html("De 1915 à 1918, il n'y a pas eu de Tour de France à cause de la Première Guerre mondiale.");
