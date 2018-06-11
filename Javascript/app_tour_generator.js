3e33a32ef662e5380d90c55d8a2fa26ccfbce190
lcarmensanchez <lea.sanchez@sciencespo.fr>
2018-06-11 23:20:41 +0200

    cleaning

--- Javascript/app_tour_generator.js
+++ Javascript/app_tour_generator.js
@@ -30,6 +30,37 @@ function searchInJson(year){
         showExplanations()
     }
 
+function showExplanations(){
+var msg = getMessages()
+  if (('img' in window || (window.DocumentTouch && document instanceof DocumentTouch))) {
+             $('div').bind('touchstart', function(){
+                 if (this.id == "maillot_jaune")
+                    $('#explanations').html(msg["explanations"].jaune);
+                else if (this.id == "maillot_pois")
+                     $('#explanations').html(msg["explanations"].pois);
+                else if (this.id == "maillot_vert")
+                     $('#explanations').html(msg["explanations"].vert);
+        });
+              };
+        $('div').bind('touchend', function(){
+            $('#explanations').html("");
+        });
+
+        $(document).ready(function(){
+            $("img").hover(function(){
+                if (this.id == "maillot_jaune")
+                    $('#explanations').html(msg["explanations"].jaune);
+                else if (this.id == "maillot_pois")
+                     $('#explanations').html(msg["explanations"].pois);
+                else if (this.id == "maillot_vert")
+                     $('#explanations').html(msg["explanations"].vert);
+            });
+            $("img").mouseleave(function(){
+                $('#explanations').html("");
+            });
+        });   
+}  
+
     $( "#generator_button" ).click(function() {
         searchInJson($( "#year" ).val());
     });
