6214a8b749c5cae19c075b05e9f2a7e00891b64c
lesanche <lesanche@student.42.fr>
2018-06-13 01:30:33 +0200

    resources > data

--- Resources/data/parser.py
+++ Resources/data/parser.py
@@ -1,10 +1,10 @@
 import csv  
 import json  
   
-f = open( '/Users/lesanche/Documents/leMonde/Tour de France - Good one (1).csv', 'rU' )   
+f = open( '/Users/lesanche/Documents/leMonde/data-TourDeFrance.csv', 'rU' )   
 reader = csv.DictReader( f, fieldnames = ( "year", "type","name", "nationality" ))  
 
 out = json.dumps([row for row in reader ] )  
-f = open( '/Users/lesanche/Documents/leMonde/parsed.json', 'w')  
+f = open( '/Users/lesanche/Documents/leMonde/data-TourDeFrance.json', 'w')  
 f.write(out)  
 print("JSON saved!");  
